ae11161ad263a9406bb4644b1637fadb31f4b2dc
Alex Page <egapxela@gmail.com>
2016-07-19 21:08:19 -0500

    Working on parser for ASC file

--- plot.py
+++ plot.py
@@ -1,9 +1,9 @@
 import sys
 
-def getascmeta (fh)
+def getascmeta (fh):
 	fh.seek(0)
 	title = fh.readline().strip()
-	if (title != "DASYLab - V 12.00.01")
+	if (title != "DASYLab - V 12.00.01"):
 		return None
 	meta = {"Worksheet name":"", "Recording date":"", "Block length":"", "Delta":"", "Number of channels":""}
 	for i in range (5):
@@ -11,4 +11,20 @@ def getascmeta (fh)
 		if (len(items)!=2 or items[0] not in meta.keys() or meta[items[0]] != ""):
 			return None
 		meta[items[0]] = items[1]
+
+	if (fh.readline().strip() != "Measurement time[s]	Disp [in]	Load [lb]"):
+		return None
 	
+	return meta
+
+if (len(sys.argv) != 2):
+	print ("Bad args")
+	exit (1)
+
+fh = open (sys.argv[1], 'r')
+meta = getascmeta (fh)
+if (meta == None):
+	print ("An error occured")
+else:
+	for item in meta.items():
+		print (item)
