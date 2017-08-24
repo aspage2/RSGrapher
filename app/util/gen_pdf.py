1d5089cdd223a0f5428a3ac44e956dbe9bad16df
Alex <egapxela@gmail.com>
2017-08-24 11:03:43 -0500

    First prerelease

--- app/util/gen_pdf.py
+++ app/util/gen_pdf.py
@@ -1,12 +1,8 @@
 
-import pylatex as tex
+import fpdf
 
-def gen_pdf(imgfile, pdfdir):
-    tex.Document()
-    doc = tex.Document(default_filepath="C:\\Users\\egapx\\Desktop\\RSG344 - My Project\\Pdfs\\")
-    doc.documentclass = tex.Command("documentclass", options=['12pt','landscape'],arguments=['article'])
-    f = tex.Figure(position="h")
-    f.add_image("C:\\Users\\egapx\\Desktop\\RSG344 - My Project\\Graphs\\S1_YL.png",width="textwidth")
-    print("WAT")
-    doc.create(f)
-    doc.generate_pdf("full")
+def gen_pdf(imgfile, pdffile):
+    pdf = fpdf.FPDF()
+    pdf.add_page("Landscape")
+    pdf.image(imgfile,w=270,y=20)
+    pdf.output(pdffile)
